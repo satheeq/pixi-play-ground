5050233228594ba956f5653c2828b7881a775b72
h_satheeq <h.satheeq@directfn.com>
2023-04-27 20:11:50 +0530

    Reverte back to 7.1 and code refactor

--- js/handler.js
+++ js/handler.js
@@ -1,146 +1,164 @@
-function start() {
-    function _createApp() {
-        // Create the renderer
-        const app = new PIXI.Application(
-            {
-                antialias: true,
-                transparent: true,
-                resolution: window.devicePixelRatio,
-                preserveDrawingBuffer: true,
-                roundPixels: false,
-                autoDensity: true,
-                width: 600,
-                height: 600,
-                backgroundColor: 0xCCCCCC
-            });
+let app;
+let baseContainer;
+let isDragging = false;
 
-        console.log('Device pixel ratio ' + window.devicePixelRatio);
+function _createApp() {
+    console.log('PIXI Version', PIXI.VERSION);
+    // Get the container
+    const htmlContainer = document.getElementById('chartContainer');
 
-        // renderer.view.style.position = 'relative'; // This is removing as a fix for the canvas dislocate issue need to be further test in all charts
-        app.view.style.display = 'block';
-        // renderer.view.style.height = '100%';
-        // renderer.view.style.width = '100%';
-        app.autoResize = true;
+    // Create the renderer
+    app = new PIXI.Application({
+            antialias: true,
+            transparent: true,
+            resolution: window.devicePixelRatio,
+            preserveDrawingBuffer: true,
+            roundPixels: false,
+            autoDensity: true,
+            resizeTo: htmlContainer,
+            backgroundColor: 0xCCCCCC,
+            sharedTicker: false,
+            autoStart: false
+        });
 
-        PIXI.settings.RESOLUTION = window.devicePixelRatio;
-        PIXI.settings.ROUND_PIXELS = false;
-        // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.LINEAR; // NEAREST- Pixelating scaling; LINEAR - Smooth scaling
-        PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.LINEAR;
+    console.log('Device pixel ratio ' + window.devicePixelRatio);
 
-        // Setting up events
-        // app.stage.interactive = true; // This is depreciated
-        app.stage.eventMode  = 'static';
+    app.view.style.display = 'block';
+    app.autoResize = true;
 
-        // app.stage.interactiveChildren = true;
-        app.stage.hitArea = app.screen;
+    PIXI.settings.RESOLUTION = window.devicePixelRatio;
+    PIXI.settings.ROUND_PIXELS = false;
+    // PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.LINEAR; // NEAREST- Pixelating scaling; LINEAR - Smooth scaling
+    PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.LINEAR;
 
-        const ticker = app.ticker;
+    // Setting up events
+    app.stage.interactive = true; // This is depreciated in 7.2
+    app.stage.eventMode = 'static'; // Used above 7.2
 
-        ticker.autoStart = false;
-        ticker.stop();
+    app.stage.interactiveChildren = true;
+    app.stage.hitArea = app.screen;
 
-        return app;
-    }
+    const ticker = app.ticker;
 
-    function onDragStart(event) {
-        app.ticker.start();
+    ticker.stop();
+    // ticker.autoStart = false;
 
-        container.eventData = event.data;
-        container.dragging = true;
-        container.dx = event.data.x - container.x;
-
-        console.log('onDragStart');
-    }
-
-    function onDragEnd() {
-        delete container.eventData;
-        container.dragging = false;
-        // app.stage.off('pointermove', onDragMoveApp);
-
-        app.ticker.stop();
-        console.log('onDragEnd');
-    }
-
-    function onDragMove(event) {
-        if (container.dragging) {
-            console.log('onDragMove', event.x);
-            container.x = event.x - container.dx;
-        }
-    }
-
-    function initChart(container) {
-        // Create window frame
-        let candleGrp = new PIXI.Graphics();
-        let lineGrp = new PIXI.Graphics();
-
-        const data = getJSONData();
-        console.error('data count', data.length);
-
-        let x = app.stage.hitArea.width - 50,
-            y = 0,
-            candW = 10,
-            candH = 30,
-            point = null,
-            bull = 0x05CE2E,
-            bear = 0xE06666,
-            color = bull;
-
-        lineGrp.moveTo(x, data[0].open + 15);
-
-
-        for (let i = 0; i < 100; i++) {
-            point = data[i];
-            x = x - candW - 4;
-            y = point.close;
-
-            color = point.close < point.open ? bear : bull;
-
-            candleGrp.beginFill(color);
-            candleGrp.lineStyle({color: 0xFFFFFF, width: 1, alignment: 0});
-            candH = (point.close - point.open) * 6;
-            candleGrp.drawRect(x, y, candW, candH);
-            candleGrp.moveTo(x, y);
-            candleGrp.endFill();
-
-            lineGrp.beginFill(0xFFF666);
-            lineGrp.lineStyle({color: 0x6FA8DC, width: 2, alignment: 0});
-            lineGrp.lineTo(x, point.open + 15);
-        }
-
-        lineGrp.closePath();
-        lineGrp.endFill();
-
-        container.addChild(candleGrp);
-        container.addChild(lineGrp);
-    }
-
-    // Create the application helper and add its render target to the page
-    // const app = new PIXI.Application({width: document.width, height: document.height});
-
-    const app = _createApp();
-    document.body.appendChild(app.view);
-
-// Add a container
-    const container = new PIXI.Container();
-    container.x = 0;
-    container.y = 0;
-
+    // Register events
+    console.log('registering events');
     app.stage.on('pointerdown', onDragStart);
-    app.stage.on('pointerup', onDragEnd);
-    app.stage.on('pointerupoutside', onDragEnd);
+    app.stage.on('pointerup', () => {
+        console.log('pointerUp');
+        onDragEnd();
+    });
+    app.stage.on('pointerupoutside', () => {
+        console.log('pointerupOutside');
+        onDragEnd();
+    });
     app.stage.on('pointermove', onDragMove);
 
-    app.stage.addChild(container);
+    app.stage.on('pointerenter', () => {
+        console.log('pointerEnter');
+    });
+    app.stage.on('pointerleave', () => {
+        console.log('pointerLeave');
+    });
 
-    initChart(container);
+    // Create the application helper and add its render target to the page
+    htmlContainer.appendChild(app.view);
+}
+
+function onDragStart(event) {
+    app.ticker.start(); // Start the ticker for a smooth update
+
+    isDragging = true;
+    baseContainer.dx = event.data.x - baseContainer.x;
+
+    console.log('onDragStart');
+}
+
+function onDragEnd() {
+    if (isDragging) {
+        isDragging = false;
+
+        app.ticker.stop(); // Stop the ticker once drag finises
+        console.log('onDragEnd');
+    }
+}
+
+function onDragMove(event) {
+    if (isDragging) {
+        baseContainer.x = event.x - baseContainer.dx;
+    }
+}
+
+function _initChart(container) {
+    // Create window frame
+    let candleGrp = new PIXI.Graphics();
+    let lineGrp = new PIXI.Graphics();
+
+    const data = getJSONData();
+    console.info('data count', data.length);
+
+    let x = app.stage.hitArea.width - 50,
+        y = 0,
+        candW = 10,
+        candH = 30,
+        point = null,
+        bull = 0x05CE2E,
+        bear = 0xE06666,
+        color = bull;
+
+    lineGrp.moveTo(x, data[0].open + 15);
+
+
+    for (let i = 0; i < 100; i++) {
+        point = data[i];
+        x = x - candW - 4;
+        y = point.close;
+
+        color = point.close < point.open ? bear : bull;
+
+        candleGrp.beginFill(color);
+        candleGrp.lineStyle({color: 0xFFFFFF, width: 1, alignment: 0});
+        candH = (point.close - point.open) * 6;
+        candleGrp.drawRect(x, y, candW, candH);
+        candleGrp.moveTo(x, y);
+        candleGrp.endFill();
+
+        lineGrp.beginFill(0xFFF666);
+        lineGrp.lineStyle({color: 0x6FA8DC, width: 2, alignment: 0});
+        lineGrp.lineTo(x, point.open + 15);
+    }
+
+    lineGrp.closePath();
+    lineGrp.endFill();
+
+    container.addChild(candleGrp);
+    container.addChild(lineGrp);
+
+
+
+    app.ticker.update(performance.now());
+    app.renderer.render(app.stage);
+
+}
+
+function _createContainer() {
+    baseContainer = new PIXI.Container();
+    app.stage.addChild(baseContainer);
+}
+
+function start() {
+    _createApp();
+    _createContainer();
+    _initChart(baseContainer);
 
     // Re-render based on a timer
     // When dragging happens, ticker enabled to update without timer
     setInterval(() => {
-        if (!container.dragging) {
-            app.ticker.update();
+        if (!isDragging) {
+            app.ticker.update(performance.now());
             app.renderer.render(app.stage);
         }
     }, 300);
-
 }
